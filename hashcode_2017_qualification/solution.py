de74dd54e01fb1a43195c9b6e3d7e619977e4215
Christoph Rist <christoph_bernd.rist@daimler.com>
2019-02-17 19:25:41 +0100

    Update solution
    Branch: master

--- hashcode_2017_qualification/solution.py
+++ hashcode_2017_qualification/solution.py
@@ -5,24 +5,27 @@
 import numpy as np
 
 from .read_input import ProblemInfo
+from .write_output import SolutionOutput
 
 
 def solve_utility(problem: ProblemInfo):
     assert problem.cache_count == problem.endpoints.shape[-1]
+    assert problem.latency_datacenter.shape[0] == problem.endpoints.shape[0]
+
+    gains = np.expand_dims(problem.latency_datacenter, axis=-1) - problem.endpoints
+    # zero gain if not connected to cache
+    gains[problem.endpoints == -1] = 0
 
     # calc utility scores per cache per video [ caches x videos ]
     # -> summarize latency gains for all cache-to-endpoint connections
 
-    scores = np.empty(shape=[problem.cache_count, len(problem.videos)], dtype=np.int32)
-
-    # problem.endpoints
-    gains = problem.endpoints
-    gains[gains == -1] = 0
-    gains = problem.endpoints * -1
-
-    cache_gains = np.sum(gains, axis=-1)
+    cache_gains = np.sum(gains, axis=0)
     assert cache_gains.shape[0] == problem.cache_count
 
-    video_scores = np.datetime_as_string()
+    video_scores = np.reciprocal(problem.videos.astype(np.float64))
 
-    return None
+    scores = np.dot(np.expand_dims(video_scores, axis=-1), np.expand_dims(cache_gains, axis=0))
+
+
+
+    return SolutionOutput()
