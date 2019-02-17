34789ef0df4f44804281e5a4b9e66a72f3137a80
David Schmidt <david.schmidt@rwth-aachen.de>
2019-02-17 23:07:23 +0100

    add numba

--- hashcode_2017_qualification/solution.py
+++ hashcode_2017_qualification/solution.py
@@ -3,6 +3,8 @@
 """Solution module."""
 
 import numpy as np
+import numba
+from numba import jit
 
 from .read_input import ProblemInfo
 from .write_output import SolutionOutput
@@ -35,13 +37,16 @@ def solve_utility(problem: ProblemInfo):
 # Program for 0-1 Knapsack problem
 # Returns the maximum value that can
 # be put in a knapsack of capacity W
+@jit(nopython=True)
 def knapSack(capacity, vidsize, time_gain, nbr_vids):
-    print('run knapSack')
+    # print('run knapSack')
 
     K = [[0 for x in range(capacity + 1)] for x in range(nbr_vids + 1)]
 
     # Build table K[][] in bottom up manner
     for i in range(nbr_vids + 1):
+        # if i % 1000 == 0:
+            # print('processing video %d' % (i-1))
         for w in range(capacity + 1):
             if i == 0 or w == 0:
                 K[i][w] = 0
@@ -66,39 +71,56 @@ def knapSack(capacity, vidsize, time_gain, nbr_vids):
             assert False
     # print(cappa)
 
-    print('knapsack problem solved')
+    # print('knapsack problem solved')
     # print(K)
-    print('selected vids for cache: ', selected_vids)
-    print('estimated time gain: ', K[nbr_vids][capacity])
+    # print('selected vids for cache: ', selected_vids)
+    # print('estimated time gain: ', K[nbr_vids][capacity])
     return K[nbr_vids][capacity], selected_vids
+
+
+@jit(nopython=True)
+def compute_latency_gains(videos, requests, cache_id, endpoints, latency_datacenter, cur_sol_state):
+    time_gain = videos * 0
+    # print(time_gain)
+    for request_idxs in zip(*np.where(requests > 0)):
+        # print('vid, ep: ', request_idxs)
+        nbr_reqs = requests[request_idxs]
+        # print('nbr: ', nbr_reqs)
+        video = request_idxs[0]
+        ep = request_idxs[1]
+        if endpoints[ep][cache_id] == -1:
+            continue
+        cur_latency = latency_datacenter[ep]
+
+        cache_conns = endpoints[ep].copy()
+        cache_conns[cache_id] = -1
+        datacenter_latency = np.ones_like(cache_conns) * latency_datacenter[ep]
+        cur_latency = np.min(np.where(np.logical_and(cache_conns >= 0, cur_sol_state[:, video]),
+                                      cache_conns, datacenter_latency))
+
+        # print('cur latency: ', cur_latency)
+        time_gain[video] += max(0, (cur_latency - endpoints[ep][cache_id]) * nbr_reqs)
+        # print('new time gain:', time_gain)
+    return time_gain
 
 
 def solve_for_single_cache(problem, cache_id, current_solution):
     print('solve for single cache %d' % cache_id)
     # current_solution.dump()
     # problem.dump()
-    time_gain = problem.videos * 0
-    # print(time_gain)
-    for request_idxs in zip(*np.where(problem.requests > 0)):
-        # print('vid, ep: ', request_idxs)
-        nbr_reqs = problem.requests[request_idxs]
-        # print('nbr: ', nbr_reqs)
-        video = request_idxs[0]
-        ep = request_idxs[1]
-        if problem.endpoints[ep][cache_id] == -1:
-            continue
-        cur_latency = problem.latency_datacenter[ep]
-        # print('cur latency: ', cur_latency)
-        for c in range(problem.cache_count):
-            if c == cache_id:
-                continue
-            if problem.endpoints[ep][c] != -1 and current_solution.state[c][video]:
-                cur_latency = min(cur_latency, problem.endpoints[ep][c])
-                # print('update cur latency: ', cur_latency)
-        time_gain[video] += max(0, (cur_latency - problem.endpoints[ep][cache_id]) * nbr_reqs)
-        # print('new time gain:', time_gain)
-    # print('time_gain: ', time_gain)
+    print('compute time gain')
+    time_gain = compute_latency_gains(
+        problem.videos,
+        problem.requests,
+        cache_id,
+        problem.endpoints,
+        problem.latency_datacenter,
+        current_solution.state
+    )
+    print('time_gain: ', time_gain)
+    print('solve knapsack')
     max_time_gain, selected_vids = knapSack(problem.cache_size, problem.videos, time_gain, len(problem.videos))
+    print('selected vids: ', selected_vids)
     assert max_time_gain >= 0
     current_solution.state[cache_id] = np.array(selected_vids, dtype=np.bool)
     return current_solution
