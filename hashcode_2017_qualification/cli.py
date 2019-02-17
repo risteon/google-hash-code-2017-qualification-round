85aeb144f202bf40d963d8b063c6fb2e8b058135
Christoph Rist <christoph_bernd.rist@daimler.com>
2019-02-17 18:11:54 +0100

    Add cli main
    Branch: master

--- hashcode_2017_qualification/cli.py
+++ hashcode_2017_qualification/cli.py
@@ -3,14 +3,26 @@
 """Console script for hashcode_2017_qualification."""
 import sys
 import click
+import os
+
+from .read_input import parse_input
+from .solution import solve_utility
 
 
 @click.command()
-def main(args=None):
-    """Console script for hashcode_2017_qualification."""
-    click.echo("Replace this message by putting your code into "
-               "hashcode_2017_qualification.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+@click.option('--problem', default='kittens.in.txt')
+def main(problem):
+
+    solution_functions = [solve_utility]
+
+    problem_obj = parse_input(os.path.join(os.path.realpath(__file__), '..', 'input', problem))
+
+    solutions = [s(problem_obj) for s in solution_functions]
+
+    # Todo write all solutions
+
+    # Todo calculate scores
+
     return 0
 
 
