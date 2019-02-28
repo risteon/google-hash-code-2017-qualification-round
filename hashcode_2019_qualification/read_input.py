87f525a44f6b814cf572ecc9cc2f599afe4ae6a1
Larissa Triess <larissa.triess@daimler.com>
2019-02-28 19:03:57 +0100

    First version read input

--- hashcode_2019_qualification/read_input.py
+++ hashcode_2019_qualification/read_input.py
@@ -1,76 +1,40 @@
-import numpy as np
-
-
 class ProblemInfo:
     def __init__(self):
-        self.videos = None  # list of video sizes
-        self.cache_size = None  # integer
-        self.cache_count = None  # integer
-        self.latency_datacenter = None  # numpy array of length number of endpoints
-        self.endpoints = None  # numpy array [num_endpoints, num_caches]; if connection, list latency, otherwise -1
-        self.requests = None  # numpy array [num_videos, num_endpoints]; number of requests, otherwise 0
+        self.num_photos = None  # integer
+        self.shapes = list()  # list
+        self.tags = list()  # list
 
     def dump(self):
-        print('dumping probleminfo')
-        print('video sizes: ', self.videos)
-        print('cache size: ', self.cache_size)
-        print('nbr caches: ', self.cache_count)
-        print('latency endpoints to datacenter: ', self.latency_datacenter)
-        print('latency from endpoint to cache: ', self.endpoints)
-        print('nbr request of vid at endpoint: ', self.requests)
+        print('dumping problem info')
+        print('number of photos: ', self.num_photos)
+        print('shapes: ', self.shapes)
 
-"""EXAMPLE
-5 2 4 3 100         5 videos, 2 endpoints, 4 request descriptions, 3 caches 100MB each.
-50 50 80 30 110     Videos 0, 1, 2, 3, 4 have sizes 50MB, 50MB, 80MB, 30MB, 110MB.
-1000 3              Endpoint 0 has 1000ms datacenter latency and is connected to 3 caches:
-0 100               The latency (of endpoint 0) to cache 0 is 100ms.
-2 200               The latency (of endpoint 0) to cache 2 is 200ms.
-1 300               The latency (of endpoint 0) to cache 1 is 300ms.
-500 0               Endpoint 1 has 500ms datacenter latency and is not connected to a cache.
-3 0 1500            1500 requests for video 3 coming from endpoint 0.
-0 1 1000            1000 requests for video 0 coming from endpoint 1.
-4 0 500             500 requests for video 4 coming from endpoint 0.
-1 0 1000            1000 requests for video 1 coming from endpoint 0.
 """
-
-
-def split_first_line(line):
-    info = line.split(' ')  # num_videos, num_endpoints, num_requests, num_caches, cache_size
-    assert len(info) == 5
-    return int(info[0]), int(info[1]), int(info[2]), int(info[3]), int(info[4])
+EXAMPLE
+Input file Description
+4
+H 3 cat beach sun
+V 2 selfie smile
+V 2 garden selfie
+H 2 garden cat
+The collection has 4 photos
+Photo 0 is horizontal and has tags [cat, beach, sun]
+Photo 1 is vertical and has tags [selfie, smile]
+Photo 2 is vertical and has tags [garden, selfie]
+Photo 3 is horizontal and has tags [garden, cat]
+"""
 
 
 def parse_input(filename):
     file = open(filename)
 
     problem_obj = ProblemInfo()
-    num_videos, num_endpoints, num_requests, num_caches, cache_size = split_first_line(file.readline())
-    problem_obj.cache_count = num_caches
-    problem_obj.cache_size = cache_size
+    problem_obj.num_photos = int(file.readline())
 
-    # video sizes
-    video_sizes = file.readline().split(' ')
-    assert len(video_sizes) == num_videos
-    problem_obj.videos = np.array(video_sizes, dtype=np.int32)
-
-    # insert dummies
-    problem_obj.latency_datacenter = np.full(shape=num_endpoints, fill_value=-1, dtype=np.int32)
-    problem_obj.endpoints = np.full(shape=[num_endpoints, num_caches], fill_value=-1, dtype=np.int32)
-    problem_obj.requests = np.zeros(shape=[num_videos, num_endpoints], dtype=np.int32)
-
-    # endpoints
-    for i in range(num_endpoints):
-        info = file.readline().split()
-        # data center latency of current endpoint
-        problem_obj.latency_datacenter[i] = int(info[0])
-        # connected caches
+    for i in range(problem_obj.num_photos):
+        info = file.readline().split(' ')
+        problem_obj.shapes.append(info[0])
         for j in range(int(info[1])):
-            info = file.readline().split()
-            problem_obj.endpoints[i][int(info[0])] = int(info[1])
-
-    # requests
-    for i in range(num_requests):
-        info = file.readline().split()
-        problem_obj.requests[int(info[0]), int(info[1])] += int(info[2])
+            problem_obj.shapes.extend(str(j))
 
     return problem_obj
